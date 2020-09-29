e108ab5217920bb84eb1f2e1613a96489beeb0e0
nathan <nwbryant@umich.edu>
2020-09-28 17:13:02 -0700

    added test for partialUpdate

--- __tests__/unit/partialUpdate.test.js
+++ __tests__/unit/partialUpdate.test.js
@@ -1,9 +1,18 @@
+const sqlForPartialUpdate = require("../../helpers/partialUpdate");
+let result;
+
+beforeEach(() => {
+  result = sqlForPartialUpdate("users", { name: "bob" }, "id", 1);
+});
+
 describe("partialUpdate()", () => {
-  it("should generate a proper partial update query with just 1 field",
-      function () {
-
-    // FIXME: write real tests!
-    expect(false).toEqual(true);
-
+  test("testing result of input query", () => {
+    expect(result).toEqual({
+      query: "UPDATE users SET name=$1 WHERE id=$2 RETURNING *",
+      values: ["bob", 1],
+    });
+  });
+  test("partial update query result object should have 2 keys", () => {
+    expect(Object.keys(result).length).toEqual(2);
   });
 });
